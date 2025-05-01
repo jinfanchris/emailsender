a86dec761a7f94d958b02125ed2971ba72420fba
Jinfan Liu <chrisckeyliu@gmail.com>
2025-05-01 11:12:12 +0800

    Using jordan-wright/email pkg

--- go.mod
+++ go.mod
@@ -1,3 +1,22 @@
 module mailsend
 
 go 1.24.2
+
+require (
+	github.com/BurntSushi/toml v1.5.0
+	github.com/jordan-wright/email v4.0.1-0.20210109023952-943e75fe5223+incompatible
+	github.com/rifflock/lfshook v0.0.0-20180920164130-b9218ef580f5
+	github.com/sirupsen/logrus v1.9.3
+	github.com/x-cray/logrus-prefixed-formatter v0.5.2
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
+	github.com/onsi/ginkgo v1.16.5 // indirect
+	github.com/onsi/gomega v1.37.0 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/sys v0.32.0 // indirect
+	golang.org/x/term v0.31.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,46 @@
 package main
 
 import (
-	"fmt"
+	// "crypto/tls"
+	"mailsend/pkg/config"
+	"mailsend/pkg/log"
+	"net/smtp"
+
+	"github.com/jordan-wright/email"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	if err := log.Setup(true); err != nil {
+		panic(err)
+	}
+
+	logrus.Infof("Hello World")
+
+	c, err := config.LoadDefaultConfig()
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	e := email.NewEmail()
+	e.From = c.Sender.Account
+	e.To = []string{c.Receiver.Account}
+	e.Subject = "Test Email from Go (TLS)"
+	e.Text = []byte("Hello,\n\nThis is a test email sent using Go with TLS.\n\nRegards,\nGo App")
+
+	err = e.Send(
+		c.Sender.SMTPServer+":587",
+		smtp.PlainAuth("", c.Sender.Account, c.Sender.Password, c.Sender.SMTPServer),
+		// &tls.Config{
+		// 	InsecureSkipVerify: true,
+		// 	ServerName:         c.Sender.SMTPServer,
+		// },
+	)
+
+	if err != nil {
+		logrus.Panic(err)
+	}
+
+	logrus.Infof("Email sent successfully to %s", c.Receiver.Account)
+
 }
